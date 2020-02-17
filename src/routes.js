97e86a010c8e61bb92766a307edbe742a5ba0848
alexgomesnet3 <alex.gomes.net3@gmail.com>
2020-02-17 06:30:02 -0300

    Cominit first changes

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { BrowserRouter, Route, Switch, Redirect } from 'react-dom';
+
+const Routes = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={() => <h1>Hello World!</h1>} />
+        </Switch>
+    </BrowserRouter>
+)
+
+export default Routes;
+
